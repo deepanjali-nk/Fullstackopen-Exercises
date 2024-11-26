0b04fe1f6d363b10b7c4b4695eeb9257254638c3
deepanjali-nk <deepanjalinagarkoti@gmail.com>
2024-11-26 10:58:04 +0545

    ex 1.11 completed

--- Part-1/unicafe/src/Statistics.jsx
+++ Part-1/unicafe/src/Statistics.jsx
@@ -1,16 +1,18 @@
+import StatisticsLine from './StatisticsLine';
 const Statistics=(props)=>{
     if (props.total === 0) {
         return <p>No feedback given</p>;
     }
     return(
         <div>
-            <h1>Statistics</h1>
-            <p>Good: {props.good}</p>
-            <p>Neutral: {props.neutral}</p>
-            <p>Bad: {props.bad}</p>
-            <p>Total: {props.total}</p>
-            <p>Average: {props.average}</p>
-            <p>Positive: {props.positive}</p>
+            <h2>statistics</h2>
+            <StatisticsLine text="good" value={props.good}/>
+            <StatisticsLine text="neutral" value={props.neutral}/>
+            <StatisticsLine text="bad" value={props.bad}/>
+            <StatisticsLine text="all" value={props.total}/>
+            <StatisticsLine text="average" value={props.average}/>
+            <StatisticsLine text="positive" value={props.positive} />
+
         </div>
     )
 }

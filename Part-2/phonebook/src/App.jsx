71a552835a659606fe2a74f8471e62e360d2d63b
deepanjali-nk <deepanjalinagarkoti@gmail.com>
2024-12-04 16:51:40 +0545

    ex 2.11

--- Part-2/phonebook/src/App.jsx
+++ Part-2/phonebook/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './Filter'
 import Person from './Person' 
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
 
+  useEffect(() => {
+    let myAxios=axios.get('http://localhost:3001/persons')
+    myAxios.then(response => {
+      console.dir(response.data);
+      setPersons(response.data);
+    })
+  }, [])
   const handleSubmit = (event) => {
     event.preventDefault();
     const  nameExists = persons.find(person => person.name === newName)

94f2780c9f3e05dfa53ca3697f4cf9c4804d3df2
deepanjali-nk <deepanjalinagarkoti@gmail.com>
2025-02-06 16:19:51 +0545

    Part4-a ex 4.3-4.7

---
+++ Part-4/blog_list/utils/list_helper.js
@@ -0,0 +1,60 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;  // Handle empty list case
+  const mostLiked = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog);
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    likes: mostLiked.likes
+  };
+};
+
+const mostBlogs = (blogs) => {
+  const authorBlogsCount = blogs.reduce((authorCount, blog) => {
+    authorCount[blog.author] = (authorCount[blog.author] || 0) + 1;
+    return authorCount;
+  }, {});
+  
+  const mostBlogsAuthor = Object.keys(authorBlogsCount).reduce((topAuthor, author) => {
+    if (authorBlogsCount[author] > authorBlogsCount[topAuthor]) {
+      return author;
+    }
+    return topAuthor;
+  });
+
+  return {
+    author: mostBlogsAuthor,
+    blogs: authorBlogsCount[mostBlogsAuthor]
+  };
+};
+const mostLikes = (blogs) => {
+  const authorLikesCount = blogs.reduce((likesCount, blog) => {
+    likesCount[blog.author] = (likesCount[blog.author] || 0) + blog.likes;
+    return likesCount;
+  }, {});
+
+  const mostLikedAuthor = Object.keys(authorLikesCount).reduce((topAuthor, author) => {
+    if (authorLikesCount[author] > authorLikesCount[topAuthor]) {
+      return author;
+    }
+    return topAuthor;
+  });
+
+  return {
+    author: mostLikedAuthor,
+    likes: authorLikesCount[mostLikedAuthor]
+  };
+};
+
+module.exports = {
+  dummy, 
+  totalLikes, 
+  favoriteBlog, mostBlogs , mostLikes
+};
